d1a96f74cb393803a3a1901672871afc7af9d413
Bibek Manandhar <manandhar.bibek01@gmail.com>
2025-04-06 15:07:17 +0545

    error message

--- src/auth/middlewares/auth.rs
+++ src/auth/middlewares/auth.rs
@@ -2,7 +2,7 @@ use axum::{extract::Request, http::StatusCode, middleware::Next, response::Respo
 
 use crate::auth::services::auth::AuthService;
 
-pub async fn auth_middleware(req: Request, next: Next) -> Result<Response, StatusCode> {
+pub async fn auth_middleware(req: Request, next: Next) -> Result<Response, (StatusCode, String)> {
     let token = req
         .headers()
         .get("Authorization")
@@ -12,8 +12,8 @@ pub async fn auth_middleware(req: Request, next: Next) -> Result<Response, Statu
     match token {
         Some(token) => match AuthService::validate_token(token) {
             Ok(_) => Ok(next.run(req).await),
-            Err(_) => Err(StatusCode::UNAUTHORIZED),
+            Err(_) => Err((StatusCode::UNAUTHORIZED, "Invalid credentials".to_string())),
         },
-        None => Err(StatusCode::UNAUTHORIZED),
+        None => Err((StatusCode::UNAUTHORIZED, "Invalid credentials".to_string())),
     }
 }
