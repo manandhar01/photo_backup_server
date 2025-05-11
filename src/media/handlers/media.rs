7fac91130da22fd3fca457d3851b2d15dacd78c6
Bibek Manandhar <manandhar.bibek01@gmail.com>
2025-05-11 13:15:25 +0545

    code refactor

--- src/media/handlers/media.rs
+++ src/media/handlers/media.rs
@@ -3,16 +3,15 @@ use axum::{
     response::Response,
     Extension, Json,
 };
-use hyper::{HeaderMap, StatusCode};
-use std::{io::SeekFrom, path::PathBuf, sync::Arc};
-use tokio::io::{AsyncReadExt, AsyncSeekExt};
-use tokio_util::io::ReaderStream;
+use hyper::HeaderMap;
+use std::sync::Arc;
 
 use crate::app::AppState;
+use crate::errors::app_error::AppError;
 use crate::media::{
     dtos::{
-        media_detail_response::MediaDetailResponse, media_list_payload::MediaListPayload,
-        media_list_response::MediaListResponse,
+        media_detail_response::MediaDetailResponse, media_download_payload::MediaDownloadPayload,
+        media_list_payload::MediaListPayload, media_list_response::MediaListResponse,
     },
     services::{
         download::DownloadService,
@@ -22,9 +21,6 @@ use crate::media::{
     },
 };
 use crate::user::models::user::User;
-use crate::{
-    errors::app_error::AppError, media::dtos::media_download_payload::MediaDownloadPayload,
-};
 
 pub async fn upload_chunk(
     State(state): State<Arc<AppState>>,
@@ -76,59 +72,7 @@ pub async fn stream_media(
         .await
         .map_err(|_| AppError::InternalServerError("Something went wrong".into()))?;
 
-    let path = PathBuf::from(media.filepath);
-
-    if !path.exists() {
-        return Err(AppError::NotFound("File not found".into()));
-    }
-
-    let file_size = match metadata.size {
-        Some(size) => size as u64,
-        None => 0,
-    };
-
-    let range_header = headers.get("range").and_then(|h| h.to_str().ok());
-
-    let (start, end) = if let Some(range) = range_header {
-        // Example: bytes=1000-
-        if let Some(range) = range.strip_prefix("bytes=") {
-            let parts: Vec<_> = range.split('-').collect();
-            let start = parts
-                .first()
-                .and_then(|s| s.parse::<u64>().ok())
-                .unwrap_or(0);
-            let end = parts
-                .get(1)
-                .and_then(|e| e.parse::<u64>().ok())
-                .unwrap_or(file_size - 1);
-            (start, end)
-        } else {
-            (0, file_size - 1)
-        }
-    } else {
-        (0, file_size - 1)
-    };
-    let chunk_size = end - start + 1;
-
-    let mut file = tokio::fs::File::open(&path).await.unwrap();
-    file.seek(SeekFrom::Start(start)).await.unwrap();
-    let stream = ReaderStream::with_capacity(file.take(chunk_size), 8192);
-
-    let mime_type = metadata
-        .mime_type
-        .unwrap_or("application/octet-stream".to_string());
-
-    let response = Response::builder()
-        .status(StatusCode::PARTIAL_CONTENT)
-        .header("Content-Type", mime_type)
-        .header("Content-Length", chunk_size.to_string())
-        .header("Accept-Ranges", "bytes")
-        .header(
-            "Content-Range",
-            format!("bytes {}-{}/{}", start, end, file_size),
-        )
-        .body(axum::body::Body::from_stream(stream))
-        .unwrap();
+    let response = DownloadService::stream_media(media, metadata, headers).await?;
 
     Ok(response)
 }
--- src/media/services/download.rs
+++ src/media/services/download.rs
@@ -1,9 +1,17 @@
-use axum::body::Body;
-use tokio::fs::File;
-use tokio::io::{AsyncReadExt, AsyncSeekExt, SeekFrom};
+use axum::{body::Body, response::Response};
+use hyper::{HeaderMap, StatusCode};
+use std::path::PathBuf;
+use tokio::{
+    fs::File,
+    io::{AsyncReadExt, AsyncSeekExt, SeekFrom},
+};
+use tokio_util::io::ReaderStream;
 
 use crate::errors::app_error::AppError;
-use crate::media::dtos::media_download_payload::MediaDownloadPayload;
+use crate::media::{
+    dtos::media_download_payload::MediaDownloadPayload,
+    models::{media::Media, media_metadata::MediaMetadata},
+};
 
 pub struct DownloadService {}
 
@@ -33,5 +41,67 @@ impl DownloadService {
         let body = Body::from(buffer[..bytes_read].to_vec());
 
         Ok((body, bytes_read))
+    }
+
+    pub async fn stream_media(
+        media: Media,
+        metadata: MediaMetadata,
+        headers: HeaderMap,
+    ) -> Result<Response, AppError> {
+        let path = PathBuf::from(media.filepath);
+
+        if !path.exists() {
+            return Err(AppError::NotFound("File not found".into()));
+        }
+
+        let mime_type = metadata
+            .mime_type
+            .unwrap_or("application/octet-stream".to_string());
+
+        let file_size = match metadata.size {
+            Some(size) => size as u64,
+            None => 0,
+        };
+
+        let range_header = headers.get("range").and_then(|h| h.to_str().ok());
+
+        let (start, end) = if let Some(range) = range_header {
+            if let Some(range) = range.strip_prefix("bytes=") {
+                let parts: Vec<_> = range.split('-').collect();
+                let start = parts
+                    .first()
+                    .and_then(|s| s.parse::<u64>().ok())
+                    .unwrap_or(0);
+                let end = parts
+                    .get(1)
+                    .and_then(|e| e.parse::<u64>().ok())
+                    .unwrap_or(file_size - 1);
+                (start, end)
+            } else {
+                (0, file_size - 1)
+            }
+        } else {
+            (0, file_size - 1)
+        };
+        let chunk_size = end - start + 1;
+
+        let mut file = tokio::fs::File::open(&path).await.unwrap();
+        file.seek(SeekFrom::Start(start)).await.unwrap();
+
+        let stream = ReaderStream::with_capacity(file.take(chunk_size), 8192);
+
+        let response = Response::builder()
+            .status(StatusCode::PARTIAL_CONTENT)
+            .header("Content-Type", mime_type)
+            .header("Content-Length", chunk_size.to_string())
+            .header("Accept-Ranges", "bytes")
+            .header(
+                "Content-Range",
+                format!("bytes {}-{}/{}", start, end, file_size),
+            )
+            .body(axum::body::Body::from_stream(stream))
+            .unwrap();
+
+        Ok(response)
     }
 }
