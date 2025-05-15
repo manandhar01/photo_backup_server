495ff188f42e6dd0e1b7d95a036f5dc46626e60d
Bibek Manandhar <manandhar.bibek01@gmail.com>
2025-05-15 21:43:05 +0545

    user handler fixed

--- src/user/routes/user.rs
+++ src/user/routes/user.rs
@@ -7,7 +7,9 @@ use std::sync::Arc;
 
 use crate::app::AppState;
 use crate::auth::middlewares::auth::auth_middleware;
-use crate::user::handlers::user::{delete_user, get_self, get_user_by_id, get_user_by_uuid};
+use crate::user::handlers::user_handler::{
+    delete_user, get_self, get_user_by_id, get_user_by_uuid,
+};
 
 pub fn user_routes(app_state: Arc<AppState>) -> Router {
     Router::new()
